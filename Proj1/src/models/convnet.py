f045556f77924f1356baf4608da3615d21a3ea65
lacoupe <leopold.coupet@epfl.ch>
2021-04-24 16:40:13 +0200

    comit test

--- Proj1/src/models/convnet.py
+++ Proj1/src/models/convnet.py
@@ -1,6 +1,5 @@
 from torch import nn
 
-
 class ConvNet(nn.Module):
     # TODO: finish the documentation for this class @lacoupe
     """[summary]
@@ -14,16 +13,20 @@ class ConvNet(nn.Module):
         # input : 2x14x14
         super().__init__()
     
+        #convolutional layers
         self.conv1 = nn.Conv2d(2,  16, kernel_size=3) #16x(14-2)x(14-2) = 16x12x12
         self.conv2 = nn.Conv2d(16, 32, kernel_size=3) #32x(12-2)x(12-2) = 32x10x10  => pooling = 32x5x5
         
+        #fully connected layers
         self.fc1 = nn.Linear(32*5*5, 128)
         self.fc2 = nn.Linear(128, 10)
         self.classifier = nn.Linear(10, 1)
         
+        #regularizers
         self.drop = nn.Dropout(0.2)
         self.pool = nn.MaxPool2d(2, 2)
-        
+
+        #activation functions
         self.relu = nn.ReLU()
         self.sigmoid = nn.Sigmoid()
 
@@ -36,6 +39,7 @@ class ConvNet(nn.Module):
         Returns:
             [type]: [description]
         """
+
         x = self.relu(self.conv1(x))
 
         x = self.pool(self.relu(self.conv2(x))) 
