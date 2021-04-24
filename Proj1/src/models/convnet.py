b2671c3986620624eb4f549c6bb1ea69e26f4043
lacoupe <leopold.coupet@epfl.ch>
2021-04-24 18:31:36 +0200

    add docstring of convnet.py

--- Proj1/src/models/convnet.py
+++ Proj1/src/models/convnet.py
@@ -1,11 +1,19 @@
 from torch import nn
 
 class ConvNet(nn.Module):
-    # TODO: finish the documentation for this class @lacoupe
-    """[summary]
+    """
+    Convolutional Network Module 
 
     Attributes:
-        conv1 (nn.Conv2d):
+        conv1 (nn.Conv2d)     : fist convolutional layer
+        conv2 (nn.Conv2d)     : second convolutional layer 
+        fc1 (nn.Linear)       : first fully connected layer 
+        fc2 (nn.Linear)       : second fully connected layer 
+        classifier (nn.Linear): last fully connected layer 
+        drop (nn.Dropout)     : dropout function
+        pool (nn.MaxPool2d)   : maxpool function
+        relu (nn.Relu)        : relu activation function
+        sigmoid (nn.Sigmoid)  : sigmoid activation function
     """
     
     def __init__(self):
@@ -13,36 +21,39 @@ class ConvNet(nn.Module):
         # input : 2x14x14
         super().__init__()
     
-        #convolutional layers
-        self.conv1 = nn.Conv2d(2,  16, kernel_size=3) #16x(14-2)x(14-2) = 16x12x12
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3) #32x(12-2)x(12-2) = 32x10x10  => pooling = 32x5x5
+        # convolutional layers
+        self.conv1 = nn.Conv2d(2,  32, kernel_size=3) # 16x(14-2)x(14-2) = 16x12x12  (=> pooling = 16x6x6)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3) # 32x(12-2)x(12-2) = 32x10x10  (=> pooling = 32x5x5)
         
-        #fully connected layers
-        self.fc1 = nn.Linear(32*5*5, 128)
+        # fully connected layers
+        self.fc1 = nn.Linear(64*5*5, 128)
         self.fc2 = nn.Linear(128, 10)
         self.classifier = nn.Linear(10, 1)
         
-        #regularizers
-        self.drop = nn.Dropout(0.2)
-        self.pool = nn.MaxPool2d(2, 2)
+        # regularizers
+        self.drop = nn.Dropout(0.1)
+        self.pool = nn.MaxPool2d(kernel_size=2)
 
-        #activation functions
+        # activation functions
         self.relu = nn.ReLU()
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        """[summary]
+        """
+        Forward pass function 
 
         Args:
-            x ([type]): [description]
+            x [float32]: input images with dimension 2x14x14
 
         Returns:
-            [type]: [description]
+            [int]: Class to predict {0,1}
         """
 
         x = self.relu(self.conv1(x))
+        x = self.drop(x)
 
-        x = self.pool(self.relu(self.conv2(x))) 
+        x = self.pool(self.relu(self.conv2(x)))
+        x = self.drop(x)
         
         x = self.relu(self.fc1(x.flatten(start_dim=1)))
         x = self.drop(x)
