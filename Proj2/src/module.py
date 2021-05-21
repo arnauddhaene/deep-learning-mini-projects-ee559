8dfd492b6b8bd66486ef91980172dd557668e907
lacoupe <leopold.coupet@epfl.ch>
2021-05-21 22:17:13 +0200

    class module.py

---
+++ Proj2/src/module.py
@@ -0,0 +1,22 @@
+class Module(object):
+    """
+    Base class for all neural network modules.
+    """
+
+    def forward(self, *input):
+        """
+        Forward pass
+        """
+        raise NotImplementedError
+
+    def backward(self, *grad):
+        """
+        Backward pass
+        """
+        raise NotImplementedError
+
+    def param(self):
+        """
+        Returns a list of parameters and their resepctive gradient
+        """
+        return []
