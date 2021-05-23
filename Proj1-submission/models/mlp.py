144a3f5fa507cf4879f753382a6d41fb89717deb
Alexander <apisarewski@yahoo.com>
2021-05-23 20:24:54 +0200

    mlp documentation

--- Proj1-submission/models/mlp.py
+++ Proj1-submission/models/mlp.py
@@ -4,13 +4,22 @@ from models.custom import SizeableModule, NamedModule, WeightInitializableModule
 
 
 class MLP(SizeableModule, NamedModule, WeightInitializableModule):
-    """[summary]
+    """
+    Multi layer perceptron
 
     Attributes:
-        fc1 ([type]): [description]
+        fc1 (nn.Linear): First fully connected layer with dimension 2x14x14 -> 128
+        fc2 (nn.Linear): Second fully connected layer with dimension  128 -> 98
+        fc3 (nn.Linear): Third fully connected layer with dimension 98 -> 49
+        fc4 (nn.Linear): Fourth fully connected layer with dimension 49 -> 98
+        classifier (nn.Linear): final classifier, fully connected linear layer with dimensions 10 -> 1
+        drop (nn.Dropout): Dropout function 
+        relu: Relu activation function 
+        sigmoid: Sigmoid activation function
     """
     
     def __init__(self):
+    """ Initialisation """
 
         super(MLP, self).__init__()
         self.fc1 = nn.Linear(2 * 14 * 14, 128)
