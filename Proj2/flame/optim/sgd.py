12982dcb6d4aa7bb6657b3a48c236fb21371c250
lacoupe <leopold.coupet@epfl.ch>
2021-05-24 09:19:44 +0200

    adam

--- Proj2/flame/optim/sgd.py
+++ Proj2/flame/optim/sgd.py
@@ -3,24 +3,23 @@ from .optimizer import Optimizer
 
 class SGD(Optimizer):
     """
-    Parameters are obtained with model.param() with model a pytorch network
-    p[0] is the parameter, p[1] is the respective gradient
+    Stochastic Gradient Descent optimizer
 
     Attributes:
         model: model
         lr: learning rate
         momentum: momentum coefficient
     """
-    def __init__(self, model, lr, momentum=0):
+    def __init__(self, model, lr, momentum=0.):
     
         self.model = model
         self.lr = lr
         self.momentum = momentum
-        self.v = self.zero_velocity()
+        self.v = self.initialize_velocity()
 
-    def zero_velocity(self):
+    def initialize_velocity(self):
         """
-        Initialize the velocity vector as zeros to match with parameter shape
+        Initialize the velocity vector as zeros according to parameter shapes
         """
         v = []
         for p in self.model.parameters():
@@ -29,7 +28,7 @@ class SGD(Optimizer):
     
     def step(self):
         """
-        The SGD momentum update parameters with regard to Sutskever definition
+        Performs a single optimization step, following Sutskever's momentum definition
         """
         for i, p in enumerate(self.model.parameters()):
             self.v[i] = self.momentum * self.v[i] + self.lr * p[1]
