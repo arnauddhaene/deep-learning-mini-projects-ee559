13379cca45e72ab9f2ee08893a8e3a5c36160531
Alexander <apisarewski@yahoo.com>
2021-05-25 11:18:27 +0200

    init funciton modification

--- Proj2/flame/nn/__init__.py
+++ Proj2/flame/nn/__init__.py
@@ -4,5 +4,6 @@ from .linear import Linear
 from .module import Module
 from .mse import MSELoss
 from .relu import ReLU
+from .tanh import Tanh
 from .sequential import Sequential
 from .dropout import Dropout
